75dcfe2efe041894903016338ace65ecec0d7e96
devendran-13070 <devendran.sr@zohocorp.com>
2024-05-25 10:56:15 +0530

    commit for initial

---
+++ shared/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    id("maven-publish")
+
+}
+
+kotlin {
+    android {
+        publishLibraryVariants("release")
+    }
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    val xcf = XCFramework()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+            xcf.add(this)
+
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.zoho.samplekmpbackend"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/zoho/kmmBackend/Utility.kt
@@ -0,0 +1,8 @@
+package com.zoho.kmmBackend
+
+object Utility {
+    fun getApiResponse():String
+    {
+        return "response from the kmp Backend"
+    }
+}
